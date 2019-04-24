cf6df96213b277f1cd4b8b558bbd872df0f58cc4
Seok Lee <seokleeuk@gmail.com>
2019-04-24 15:19:55 -0400

    Update model_r0.py

--- model_r0.py
+++ model_r0.py
@@ -12,7 +12,8 @@ import random
 
 samples = []
 d = os.getcwd()
-with open(d+'/data/driving_log.csv') as csvfile:
+#with open(d+'/data/driving_log.csv') as csvfile:
+with open(d+'/data_sample/data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
@@ -30,7 +31,8 @@ def generator(samples, batch_size=32):
             images = []
             measurements = []
             for batch_sample in batch_samples:
-                name = d+'/data/IMG/'+batch_sample[0].split('/')[-1]
+                name = d+'/data_sample/data/IMG/'+batch_sample[0].split('/')[-1]
+                #name = d+'/data/IMG/'+batch_sample[0].split('/')[-1]
                 image = ndimage.imread(name)
                 measurement = float(batch_sample[3])
                 images.append(image)
@@ -93,15 +95,21 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 
+from keras.layers import BatchNormalization, Dropout
+
 model = Sequential()
 model.add(Lambda(lambda x: x/127.5 - 1.,
         input_shape=(row, col, ch),
         output_shape=(row, col, ch)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
+#model.add(BatchNormalization()) # overfitting
 model.add(Conv2D(24,(5,5),strides=(2,2),activation="relu"))
+#model.add(Conv2D(24,(5,5),strides=(2,2),activation="relu"))
+#model.add(BatchNormalization()) # overfitting
 model.add(Conv2D(36,(5,5),strides=(2,2),activation="relu"))
+#model.add(BatchNormalization()) # overfitting
 model.add(Conv2D(48,(5,5),strides=(2,2),activation="relu"))
-
+#model.add(BatchNormalization()) # overfitting
 model.add(Conv2D(64,(3,3),activation="relu"))
 model.add(Conv2D(64,(3,3),activation="relu"))
 
@@ -112,9 +120,17 @@ model.add(Conv2D(64,(3,3),activation="relu"))
 
 model.add(Flatten())
 
+from keras.regularizers import l1 # overfitting
+
 model.add(Dense(100))
+#model.add(Dense(100),kernel_regularizer=l1(0.001)) # overfitting
+model.add(Dropout(0.2)) # overfitting
 model.add(Dense(50))
+#model.add(Dense(50),kernel_regularizer=l1(0.001)) # overfitting
+model.add(Dropout(0.2)) # overfitting
 model.add(Dense(10))
+#model.add(Dense(10),kernel_regularizer=l1(0.001)) # overfitting
+model.add(Dropout(0.2)) # overfitting
 model.add(Dense(1))
 #model.add(Dense(120))
 #model.add(Dense(84))
@@ -122,11 +138,28 @@ model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
 #model.fit(X_train,y_train,validation_split = 0.2,shuffle=True,epochs = 2)
-model.fit_generator(train_generator, \
+history_object = model.fit_generator(train_generator, \
             steps_per_epoch=math.ceil(len(train_samples)/batch_size), \
             validation_data=validation_generator, \
             validation_steps=math.ceil(len(validation_samples)/batch_size), \
             epochs=5, verbose=1)          
 model.save('model.h5')
 
-    
+from keras.models import Model
+import matplotlib.pyplot as plt
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+#plt.plot([1,2,3,4])
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('test1.png')
+#plt.show()
+        
